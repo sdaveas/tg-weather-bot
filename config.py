bc713ef4d8df9c85ae6f8c4702f90f24f23a4287
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2023-10-28 14:28:35 +0300

    add cronjob

--- config.py
+++ config.py
@@ -1,14 +1,24 @@
 import os
 import dotenv
 
-ENV_FILE='local.env'
+ENV_FILE = "local.env"
 
 if os.path.exists(ENV_FILE):
     dotenv.load_dotenv(ENV_FILE)
 
 # Telegram
-TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
-TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
+TG_CHAT_ID = os.getenv("TG_CHAT_ID")
 
 # OpenWeatherMap
-OW_API_KEY = os.getenv('OW_API_KEY')
+OW_API_KEY = os.getenv("OW_API_KEY")
+
+# Google Calendar
+GC_API_KEY = os.getenv("GC_API_KEY")
+GC_ID = os.getenv("GC_ID")
+GC_EVENT_KEYWORDS = os.getenv("GC_EVENT_KEYWORDS")
+GC_TIMEDELTA_HOURS = os.getenv("GC_TIMEDELTA_HOURS")
+
+# Location
+LAT = os.getenv("LAT")
+LON = os.getenv("LON")
